716c2f6fcde674efb3aa864f2084898a52881e61
4b0g0090 <4b0g0090@stust.edu.tw>
2023-11-09 12:00:08 +0800

    test2.py
    homework5

---
+++ test2.py
@@ -0,0 +1,81 @@
+#1
+#def  value(name,age):
+#   print('name:',name,'age:',age)
+
+#value('jay',18)
+
+#2
+#def func1 (a1,a2,a3):
+#    print('列印值')
+#    print(a1)
+#    print(a2)
+#    print(a3)
+# call function with 3 arguments
+
+#func1(20, 40, 60)
+
+# call function with 2 arguments
+#def func1 (a1,a2):
+#    print('列印值')
+#    print(a1)
+#    print(a2)
+    
+#func1(80, 100)
+
+#3
+#def calculation(a, b):
+#    addition = a + b
+#    subtraction = a - b
+   
+#    return addition, subtraction
+#res = calculation(40, 10)
+#print(res)
+
+#4
+#def showEmployee (name,money=9000):
+#    print('name:',name,'money:',money)
+
+#showEmployee("Ben", 12000)
+#showEmployee("Jessa")
+
+#5
+# def A(a, b):
+#     square = a ** 2
+
+#     def B(a, b):
+#         return a + b
+
+#     add = B(a, b)
+   
+#     return add + 5
+
+# result = A(5, 10)
+# print(result)
+
+
+#6
+# def factorial_loop (n):
+#    factor = 0
+#    for i in range(1,n+1):
+#        factor += i
+#    return factor
+# print(factorial_loop(10))
+
+
+#7
+# def display_student(name, age):
+#    print(name, age)
+
+# display_student("Emma", 26)
+
+# showStudent=display_student
+# showStudent("Emma", 26)
+
+
+#8
+#print(list(range(4, 30, 2)))
+
+
+#9
+#x = [4, 6, 8, 24, 12, 2]
+#print(max(x))
